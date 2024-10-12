bd282ded9c1d859ac84e3637ce39be84eb5e2c46
Sonia-bit <sarasonia.kad@gmail.com>
2024-10-12 19:42:23 +0530

    Adding project to GitHub repository

---
+++ RenamingUtility/RenamingUtility/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.IO;
+using System;
+
+namespace RenamingUtility
+
+{
+    class Program
+
+    {
+        static void Main(string[] args) 
+        
+        {
+
+            Console.WriteLine("Renaming utility has started executing...");
+
+            if (args.Length>1) 
+            { 
+            string oldFileName = args[0];
+                string newFileName = args[1];
+
+                if(File.Exists(oldFileName) ) 
+                {
+                
+                    File.Move(oldFileName, newFileName);
+                    Console.WriteLine("The file name has been changed successfully");
+
+                }
+
+                else 
+                {
+                    Console.WriteLine($"The file name {oldFileName} does not exist");
+                }
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Not enough arguments to rename");
+            
+            
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
